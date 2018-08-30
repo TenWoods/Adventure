74d7072f3b4e1df88efa0160485cc7c21fd81d2b
DESKTOP-SB6JG5T\十木 <yangwx0905@sina.com>
2018-08-30 13:17:33 +0800

    start

---
+++ Assets/Scripts/ShipMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMove : MonoBehaviour 
+{
+	[SerializeField]
+	private float speed;
+	private Rigidbody rb;
+
+	private void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	private void Update() 
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		Debug.Log(h);
+		Debug.Log(v);
+		rb.velocity = new Vector3(h, v, 0) * speed;
+	}
+}
