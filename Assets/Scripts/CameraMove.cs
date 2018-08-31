09c3a969bf0092a11ab8bb39ab10fe523b1d1a71
十木 <yangwx0905@sina.com>
2018-08-31 15:40:44 +0800

    鼠标相关

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMove : MonoBehaviour {
+public class CameraMove : MonoBehaviour 
+{
+	[SerializeField]
+	private float speed;
+	[SerializeField]
+	private float maxOffset;
+	private Vector3 oriLength;
+	private Vector3 lastMousePos;
+	public GameObject ship;
 
-	// Use this for initialization
-	void Start () {
-		
+	private void Start() 
+	{
+		oriLength = transform.position - ship.transform.position;
+		lastMousePos = Vector3.zero;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	private void FixedUpdate() 
+	{
+		Vector3 moveDir = ship.transform.position + oriLength;
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+		Vector3 offset = transform.position -  mousePos;
+		Debug.Log(offset);
+		if (Mathf.Abs(offset.x) >= maxOffset)
+		{
+			if (offset.x < 0)
+			{
+				offset.x = -maxOffset;
+			}
+			else
+			{
+				offset.x = maxOffset;
+			}
+		}
+		moveDir.x += offset.x;
+		transform.position = moveDir;
 	}
 }
--- Assets/Scripts/ShipMove.cs
+++ Assets/Scripts/ShipMove.cs
@@ -17,8 +17,6 @@ public class ShipMove : MonoBehaviour
 	{
 		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
-		Debug.Log(h);
-		Debug.Log(v);
 		rb.velocity = new Vector3(h, v, 0) * speed;
 	}
 }
