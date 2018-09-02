ef418c861a11008f4e257110574a2740b5ccc3d5
十木 <yangwx0905@sina.com>
2018-09-02 18:52:23 +0800

    同步

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour 
+{
+	[SerializeField]
+	private float speed = 1;
+	private Vector3 target;
+	private Rigidbody2D rb;
+	
+	private void Start() 
+	{
+		rb = GetComponent<Rigidbody2D>();
+		Vector3 dir = target - transform.position;
+		rb.velocity = dir.normalized * speed;
+	}
+
+	public Vector3 Target 
+	{
+		set
+		{
+			target = value;
+		}
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Enter");
+		if (other.tag == "Edge")
+		{
+			Destroy(this);
+		}
+	}
+}
--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -6,16 +6,37 @@ public class Ship : MonoBehaviour
 {
 	[SerializeField]
 	private float speed;
-	private Rigidbody rb;
+	private Rigidbody2D rb;
+	public Transform[] shootPoints;
+	public GameObject bullet_Prefab;
 
 	private void Start() 
 	{
-		rb = GetComponent<Rigidbody>();
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate() 
+	{
+		Move();
 	}
 
 	private void Update() 
 	{
-		Move();
+		Shoot();
+	}
+
+	private void Shoot()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			GameObject bullet = null;
+			for(int i = 0; i < 2; i++)
+			{
+				bullet = GameObject.Instantiate(bullet_Prefab, shootPoints[i].position, shootPoints[i].rotation);
+				bullet.GetComponent<Bullet>().Target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+				Debug.Log("Instantiate" + i);
+			}
+		}
 	}
 
 	private void Move()
